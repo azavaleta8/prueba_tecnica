ee42b472823a9d44f4155dfd635dc9c424da3244
abelzavaleta <abelzavaleta08@gmail.com>
2024-04-28 10:33:35 -0400

    Backend: cors conf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'processDataApp'
 ]
 
@@ -37,6 +38,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Agrega aquí el origen del front-end
 ]
 
 REST_FRAMEWORK = {
